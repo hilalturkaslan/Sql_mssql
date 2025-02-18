166b0c6bcbb8567df596a5008c5cfaa394651ab5
hilalturkaslan <113252258+hilalturkaslan@users.noreply.github.com>
2025-02-18 16:39:08 +0300

    Add files via upload

---
+++ SQLQuery14.sql
@@ -0,0 +1,18 @@
+create view nrt_sales
+as
+
+select * from [order_details$] od
+inner join orders$ o on od.orderid = o.orderid
+
+CREATE VIEW nrt_sales AS
+SELECT 
+    od.orderid,
+    od.productid,
+    od.quantity,
+    od.unitprice,
+    od.discount,
+    o.customerid,
+    o.orderdate,
+    o.shipcountry
+FROM [order_details$] od
+INNER JOIN orders$ o ON od.orderid = o.orderid
